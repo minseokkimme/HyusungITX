c9bcca1d830dbaf5c412bbacb4ba80722a375994
inseon park <sesoon497@naver.com>
2022-08-16 10:14:38 +0900

    Revert "edit background picture, modify cors"
    This reverts commit 58a80b27ead07db59c4bb29053d794bce4952000.

--- M_PointMall/app.js
+++ M_PointMall/app.js
@@ -10,9 +10,6 @@ var users = require('./routes/users');
 
 var app = express();
 
-const cors = require('cors');
-app.use(cors());
-
 // view engine setup
 app.use(express.static(__dirname + '../public'));
 app.set('views', path.join(__dirname, 'views'));
